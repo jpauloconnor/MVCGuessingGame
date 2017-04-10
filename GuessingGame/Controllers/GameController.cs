1793a4ef34baad1dd2676eff5f8f2582a8b6d3f1
Paul O'Connor <jamespauloconnor@gmail.com>
2017-04-10 16:49:02 -0400

    Initial commit.

---
+++ GuessingGame/Controllers/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuessingGame.Models;
+
+namespace GuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+          public ActionResult Index()
+        {
+            Session["Answer"] = new Random().Next(1, 10);
+            return View();
+        }
+
+        //This is a shorthand C#6 way to write a method.
+        private bool GuessWasCorrect(int guess) =>
+            guess == (int)Session["Answer"];
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(GameModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Win = GuessWasCorrect(model.Guess);
+            }
+            return View(model);
+        }
+    }
+}
